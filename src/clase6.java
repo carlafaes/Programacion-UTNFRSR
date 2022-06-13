109ffc9613a6e2ece07e6bfe0d98a5edf59858e2
carlafaes <carlafaes18@hotmail.com>
2022-06-13 17:32:20 -0300

    javaclase 6

---
+++ src/clase6.java
@@ -0,0 +1,16 @@
+public class clase6 {
+
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos booleanos
+        boolean varBolean = true;
+        System.out.println("varBolean = " + varBolean);
+
+        if(varBolean){
+            System.out.println("varBolean es verdadero");
+        }
+        else{
+            System.out.println("varBolean es falso");
+        }
+    }
+
+}
