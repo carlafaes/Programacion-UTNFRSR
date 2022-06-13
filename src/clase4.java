77f6d55d07ae6f6a24ba90a6beb8cc34f8b28041
carlafaes <carlafaes18@hotmail.com>
2022-06-13 16:39:01 -0300

    clase4

---
+++ src/clase4.java
@@ -0,0 +1,38 @@
+public class clase4 {
+    public static void main(String[] args) throws Exception {
+        byte numEnteroByte=(byte)129;
+        System.out.println("numEnteroByte = "+numEnteroByte);
+        System.out.println("Valor minimo del byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo del byte:" + Byte.MAX_VALUE);
+        
+
+        short numEnteroShort=(short)1000;
+        System.out.println("numEnteroShort = "+numEnteroShort);
+        System.out.println("Valor minimo del short:" + Short.MIN_VALUE);
+        System.out.println("Valor maximo del short:" + Short.MAX_VALUE);
+
+        int numEnteroInt=1000;
+        System.out.println("numEnteroInt = "+numEnteroInt);
+        System.out.println("Valor minimo del int:" + Integer.MIN_VALUE);
+        System.out.println("Valor maximo del int:" + Integer.MAX_VALUE);
+
+        long numEnteroLong=1000L;
+        System.out.println("numEnteroLong = "+numEnteroLong);
+        System.out.println("Valor minimo del long:" + Long.MIN_VALUE);
+        System.out.println("Valor maximo del long:" + Long.MAX_VALUE);
+
+        float numEnteroFloat=1000.0f;
+        System.out.println("numEnteroFloat = "+numEnteroFloat);
+        System.out.println("Valor minimo del float:" + Float.MIN_VALUE);
+        System.out.println("Valor maximo del float:" + Float.MAX_VALUE);
+
+        double numEnteroDouble=1000.0;
+        System.out.println("numEnteroDouble = "+numEnteroDouble);
+        System.out.println("Valor minimo del double:" + Double.MIN_VALUE);
+        System.out.println("Valor maximo del double:" + Double.MAX_VALUE);
+
+        char Char='a';
+        System.out.println("Char = "+ Char);
+
+    }
+}
