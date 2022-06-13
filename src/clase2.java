41cf282e481c08cbde9b3e49e43492c77e95e547
carlafaes <carlafaes18@hotmail.com>
2022-06-13 14:35:56 -0300

    j

---
+++ src/clase2.java
@@ -0,0 +1,16 @@
+public class clase2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo desde Java!");
+
+        int miVariable=10;
+        System.out.println(miVariable);
+        miVariable=5;
+        System.out.println(miVariable);
+
+        //Tipo string
+        String miVariableCadena="Bienvenido a Java";
+        System.out.println(miVariableCadena);
+        miVariableCadena= "sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+    }
+}
