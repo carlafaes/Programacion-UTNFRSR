0e2bc5ca333cade65740c395ba242793bb9b47c4
carlafaes <carlafaes18@hotmail.com>
2022-06-14 15:04:01 -0300

    clase6

--- src/clase6.java
+++ src/clase6.java
@@ -11,6 +11,26 @@ public class clase6 {
         else{
             System.out.println("varBolean es falso");
         }
+
+        //algoritmo ¿Es mayor de edad?
+        var edad = 28;
+        //var adulto= edad >= 18;
+
+        if(edad >= 18){
+            System.out.println("Es mayor de edad");
+        }
+        else{
+            System.out.println("No es mayor de edad");  
+        }
+
+
+
+        //conversion de tipos primitivos
+        var _edad= Integer.parseInt("25");
+        System.out.println("_edad = " + _edad);
     }
 
 }
+
+
+
