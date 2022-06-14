e61519a7a84be262174afe2567a5caec07936d6b
carlafaes <carlafaes18@hotmail.com>
2022-06-14 16:48:47 -0300

    operadores unarios

--- src/clase8.java
+++ src/clase8.java
@@ -13,5 +13,23 @@ public class clase8 {
         System.out.println("varE = " + varE);//se incrementa en la unidad
         System.out.println("varF = " + varF);//va a sumar uno
 
+        //Postincremento(el simbolo va despues del valor)
+        var varG= 3;
+        var varH=varG++;//primero el valor de la variable y luego el incremento
+        System.out.println("varG = " + varG);
+        System.out.println("varH = " + varH);
+
+        //operadores unarios de decremento:Predecremento
+        var varI=9;//se va a modificar su valor
+        var varJ= --varI;
+        //Primero se decrementa el valor de la variable y luego se lee
+        System.out.println("varI = " + varI);//se decrementa en la unidad
+        System.out.println("varJ = " + varJ);//va a restar uno
+
+        //Postdecremento(el simbolo va despues del valor)
+        var varK= 3;
+        var varL=varK--;//primero el valor de la variable y luego el decremento
+        System.out.println("varK = " + varK);
+        System.out.println("varL = " + varL);
     }
 }
