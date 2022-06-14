adf86a76a0e488b8d4cc47124c0283155c9ab935
carlafaes <carlafaes18@hotmail.com>
2022-06-14 16:03:20 -0300

    f

--- src/clase7.java
+++ src/clase7.java
@@ -1,8 +1,23 @@
 public class clase7 {
     public static void main(String[] args) throws Exception {
         //operadores de asignacion
-        int varNum1=1, varNum2=4;
+        int varNum1=1, varNum2=4;//asignacion de valores
         int varNum3= varNum1 + 6 - varNum2;
         System.out.println("varNum3 = " + varNum3);
+
+        varNum1 += 1;
+        System.out.println("suma de varNum1 = " + varNum1);
+
+        varNum1 -= 1;
+        System.out.println("resta de varNum1 = " + varNum1);
+
+        varNum1 *= 1;
+        System.out.println("multiplicacion de varNum1 = " + varNum1);
+
+        varNum1 /= 1;
+        System.out.println("division de varNum1 = " + varNum1);
+
+        varNum1 %= 1;
+        System.out.println("modulo de varNum1 = " + varNum1);
     }
 }
