1031180b9d35d9909cec1653033792ff448b2c81
carlafaes <carlafaes18@hotmail.com>
2022-06-14 15:58:24 -0300

    operadores de asignacion

--- src/clase6.java
+++ src/clase6.java
@@ -84,6 +84,8 @@ public class clase6 {
         
     }
 
+    
+
 }
 
 
---
+++ src/clase7.java
@@ -0,0 +1,8 @@
+public class clase7 {
+    public static void main(String[] args) throws Exception {
+        //operadores de asignacion
+        int varNum1=1, varNum2=4;
+        int varNum3= varNum1 + 6 - varNum2;
+        System.out.println("varNum3 = " + varNum3);
+    }
+}
